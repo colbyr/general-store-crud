4625e25940c14dad60b93850abbd86fd94f6c749
Colby Rabideau <idontknow5691@gmail.com>
2016-07-14 13:56:47 -0400

    jest-check types ish

---
+++ src/constants/CRUDTypes.js
@@ -0,0 +1,5 @@
+/* @flow */
+export const CREATE = 'CREATE';
+export const READ = 'READ';
+export const UPDATE = 'UPDATE';
+export const DELETE = 'DELETE';
---
+++ src/constants/ProgressEvents.js
@@ -0,0 +1,4 @@
+/* @flow */
+export const BEGAN = 'BEGAN';
+export const FAILED = 'FAILED';
+export const SUCCEEDED = 'SUCCEEDED';
