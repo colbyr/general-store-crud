7d2e1ddfd2a9bc8595493214abc59e54519dc31e
Colby Rabideau <idontknow5691@gmail.com>
2016-07-14 14:42:09 -0400

    more tests

--- src/__tests__/ActionTypes-test.js
+++ src/__tests__/ActionTypes-test.js
@@ -3,21 +3,40 @@ jest.unmock('../ActionTypes');
 import { joinSegments, normalizeSegment } from '../ActionTypes';
 import { check, gen } from 'jest-check';
 
+const nonEmptyString = gen.suchThat(
+  (str) => str.trim().length !== 0,
+  gen.string
+);
+
 describe('ActionTypes', () => {
   describe('joinSegments', () => {
+    check.it(
+      'converts many strings into actions',
+      [gen.array(gen.notEmpty(gen.alphaNumString), 1, 5)],
+      (segments) => expect(
+        joinSegments(...segments)
+      ).toBe(
+        segments.join('_').toUpperCase()
+      )
+    );
+
     it('converts string arguements to actions', () => {
       expect(joinSegments('one', 'two', 'three')).toBe('ONE_TWO_THREE');
     });
   });
 
   describe('normalizeSegment', () => {
-    check.it('accepts and returns a string', [gen.notEmpty(gen.string)], (str) => {
-      expect(typeof normalizeSegment(str)).toBe('string');
-    });
+    check.it(
+      'accepts and returns a string',
+      [nonEmptyString],
+      (str) => expect(typeof normalizeSegment(str)).toBe('string')
+    );
 
-    check.it('never contains lowercase letters', [gen.notEmpty(gen.string)], (str) => {
-      expect(normalizeSegment(str)).toMatch(/[^a-z]/);
-    });
+    check.it(
+      'contains only uppercase letters, numbers, and _s',
+      [nonEmptyString],
+      (str) => expect(normalizeSegment(str)).toMatch(/^[A-Z0-9_]+$/)
+    );
 
     it('converts to upper case', () => {
       expect(normalizeSegment('omgomgwow')).toBe('OMGOMGWOW');
