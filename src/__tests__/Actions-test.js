7197ce659c81163d542cf277a48f95f20632eb2a
Colby Rabideau <idontknow5691@gmail.com>
2016-07-15 10:42:16 -0400

    some more action running stuff ;

--- src/__tests__/Actions-test.js
+++ src/__tests__/Actions-test.js
@@ -3,22 +3,22 @@ jest.unmock('../Actions');
 
 describe('Actions', () => {
   describe('makeCreateAction', () => {
-    it('binds the correct params to runAction', () => {
+    xit('binds the correct params to runAction', () => {
     });
   });
 
   describe('makeReadAction', () => {
-    it('binds the correct params to runAction', () => {
+    xit('binds the correct params to runAction', () => {
     });
   });
 
   describe('makeUpdateAction', () => {
-    it('binds the correct params to runAction', () => {
+    xit('binds the correct params to runAction', () => {
     });
   });
 
   describe('makeDeleteAction', () => {
-    it('binds the correct params to runAction', () => {
+    xit('binds the correct params to runAction', () => {
     });
   });
 });
