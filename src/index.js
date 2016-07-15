a3b76fd43540c06bac17b4499f5667cdcaf8071c
Colby Rabideau <idontknow5691@gmail.com>
2016-07-15 13:49:58 -0400

    index with es6 exports

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import * as Actions from './Actions';
+import * as ActionTypes from './ActionTypes';
+
+export const makeActionTypes = ActionTypes.makeActionTypes;
+export const makeCreateAction = Actions.makeCreateAction;
+export const makeReadAction = Actions.makeReadAction;
+export const makeUpdateAction = Actions.makeUpdateAction;
+export const makeDeleteAction = Actions.makeDeleteAction;
